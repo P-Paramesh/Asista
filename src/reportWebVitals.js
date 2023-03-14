72a7f21fb3b4e8444f54ab6e839a5ad169620ccc
P.Paramesh <95079545+P-Paramesh@users.noreply.github.com>
2023-03-14 12:48:07 +0530

    Update reportWebVitals.js

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -4,10 +4,5 @@ const reportWebVitals = onPerfEntry => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
-};
 
 export default reportWebVitals;
