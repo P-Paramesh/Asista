e4098d4c82f6e1f39435917518553df1e7e101df
P.Paramesh <95079545+P-Paramesh@users.noreply.github.com>
2023-04-03 13:42:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-Document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+<React.StrictMode>
+  <App />
+</React.StrictMode>
+)
+
+reportWebVitals();
+
