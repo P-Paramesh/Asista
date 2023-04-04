ab98bf9d08d38644952aaf5ede0e59e0dc541d84
P.Paramesh <95079545+P-Paramesh@users.noreply.github.com>
2023-04-04 13:34:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals';                         
+import {ChakraProvider }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
