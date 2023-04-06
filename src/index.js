68e368b64f3a658b39b989f0c837149470e83199
P.Paramesh <95079545+P-Paramesh@users.noreply.github.com>
2023-04-06 14:38:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,10 @@ import reportWebVitals from './reportWebVitals';
 import {ChakraProvider, extendTheme } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
 <React.StrictMode>
   <App />
 </React.StrictMode>
 )
-
 reportWebVitals();
 
